ddbccd35a451f534dc70e49629534959f0793881
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-02-22 16:12:11 -0300

    Criado formulario em Project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
-import NewProjects from './components/pages/NewProjects';
+import NewProject from './components/pages/NewProject';
 import Container from './components/layout/Container';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
@@ -16,9 +16,9 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/newproject" element={<NewProjects />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
       <Footer />
--- src/components/pages/NewProjects.jsx
+++
@@ -1,5 +0,0 @@
-function NewProjects() {
-  return <h1>Novos Projetos</h1>;
-}
-
-export default NewProjects;
